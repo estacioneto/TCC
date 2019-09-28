fac0be52ebc2187dfef6abe3488cd985a85009b6
Estácio Pereira <estacio.pereira@gmail.com>
2019-09-28 19:09:55 -0300

    Após vários testes com o workbox, estamos no caminho viável: Implementar o próprio event handler

--- react/src/app/index.tsx
+++ react/src/app/index.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react'
 const App: React.FC<{}> = () => {
   const [data, setData] = useState<string | number>('Loading...')
   const [counter, setCounter] = useState(1)
+  const [stop, setStop] = useState(false)
 
   useEffect(() => {
     setTimeout(() => setCounter(counter + 1), 500)
@@ -12,6 +13,17 @@ const App: React.FC<{}> = () => {
     <div className="flex flex-column justify-center items-center vh-100">
       <div>Hello world!</div>
       <div>(counter: {counter})</div>
+      <button
+        onClick={() =>
+          fetch('http://localhost:8080/api/counters')
+            .then(res => res.json())
+            .then(response => {
+              console.log(response)
+            })
+        }
+      >
+        Fetch!
+      </button>
     </div>
   )
 }
